0d280a6b64527642e4d67d498520d5da0ee9acbc
zhangchuanchuan <543749092@qq.com>
2016-04-22 23:42:11 +0800

    添加pull_to_refresh控件

--- app/src/main/java/com/stream/wangxiang/fragment/HomeFragment.java
+++ app/src/main/java/com/stream/wangxiang/fragment/HomeFragment.java
@@ -5,17 +5,33 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.stream.wanxiang.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 首页的fragment
  * Created by 张川川 on 2016/4/22.
  */
 public class HomeFragment extends BaseFragment {
+
+    private ListView mNewsList;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mNewsList = (ListView) view.findViewById(R.id.news_list);
+
+        return view;
     }
+
+
+
 }
